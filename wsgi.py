c5291d5d3e1598308aa88191778f635a5e93678e
Aakarsh Kumar <aakarsh2504@gmail.com>
2021-06-06 18:29:16 +0530

    first commit

--- wsgi.py
+++ wsgi.py
@@ -9,7 +9,7 @@ def findAvailability(pincode,date):
         'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36'}
 
     result = requests.get(URL, headers=header)
-    if result.status_code==304:
+    if result.status_code==302:
         response_json = result.json()
         data = response_json["sessions"]
         return data
