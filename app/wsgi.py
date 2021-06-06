7a4b5ed96cc68ace77019da91da4725fab81a48b
Aakarsh Kumar <aakarsh2504@gmail.com>
2021-06-06 14:51:04 +0530

    first commit

--- app/wsgi.py
+++ app/wsgi.py
@@ -1,7 +1,23 @@
 from flask import Flask, render_template ,request, redirect
-#from requests.api import request
+import requests
+import time
+def findAvailability(pincode,date):
+    URL = 'https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode={}&date={}'.format(
+        pincode,date)
+
+    header = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36'}
+
+    result = requests.get(URL, headers=header)
+    if result.status_code==200:
+        response_json = result.json()
+        data = response_json["sessions"]
+        return data
+    else:
+        return False
 app = Flask(__name__,template_folder='templates')
-from Cowin_Slots import *
+
+global URL, header
 
 @app.route('/')
 def index():
